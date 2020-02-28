20d1cf4195c2055f0089e7e51d34c14f3bd41f20
MinoMunMun <wunonglin@gmail.com>
2020-02-29 00:02:58 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# BilibiliAlbumDownload
+
+## How to use
+
+#### Download
+```text
+git clone https://github.com/MunMunMiao/BilibiliAlbumDownload.git
+```
+
+#### Install dependencies
+```text
+npm install
+```
+
+#### Quick start
+```text
+node bilibili.js -u 8366990
+```
+
+## Options
+```text
+Usage: index [options]
+
+Options:
+  -u, --uid <number>        Bilibili uid
+  -d, --directory <string>  Output directory (default: "./")
+  -t, --threads <number>    Download threads (default: 5)
+  -o, --output-file         Output files
+  -h, --help                output usage information
+```
---
+++ bilibili.js
@@ -0,0 +1,143 @@
+const fs = require('fs')
+const path = require('path')
+const axios = require('axios')
+const commander = require('commander')
+const { from } = require('rxjs')
+const { mergeMap } = require('rxjs/operators')
+
+commander
+    .requiredOption('-u, --uid <number>', 'Bilibili uid')
+    .requiredOption('-d, --directory <string>', 'Output directory', './')
+    .option('-t, --threads <number>', 'Download threads',5)
+    .option('-o, --output-file', 'Output files')
+
+commander.parse(process.argv)
+
+let bilibiliUid = null
+const userAgent = `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36`
+const pageSize = 30
+let thread = 5
+let outputFile = false
+let directory = null
+
+if (commander.uid){
+    bilibiliUid = commander.uid
+}
+if (commander.directory){
+    directory = path.join(commander.directory, bilibiliUid)
+}
+if (commander.threads){
+    thread = Number(commander.threads)
+}
+if (commander.outputFile){
+    outputFile = true
+}
+
+console.log(`Input config`)
+console.log(`-------------------`)
+console.log(`uid: ${ bilibiliUid }`)
+console.log(`output directory: ${ directory }`)
+console.log(`thread: ${ thread }`)
+console.log(`output files: ${ outputFile }`)
+console.log(`-------------------`)
+
+async function findPictures(){
+    const count = await axios.get('https://api.vc.bilibili.com/link_draw/v1/doc/upload_count', {
+        headers: {
+            'User-Agent': userAgent
+        },
+        params: {
+            uid: bilibiliUid
+        },
+        transformResponse: data => {
+            const resp = JSON.parse(data)
+            return resp.data.all_count
+        }
+    })
+
+    const quest = []
+
+    for (let index = 0; index < Math.ceil(count.data / pageSize); index++){
+        console.log(`Analyze page: ${ (index + 1) }/${ Math.ceil(count.data / pageSize) }`)
+
+        quest.push(axios.get('https://api.vc.bilibili.com/link_draw/v1/doc/doc_list', {
+            headers: {
+                'User-Agent': userAgent
+            },
+            params: {
+                uid: bilibiliUid,
+                page_num: index,
+                page_size: pageSize,
+                biz: 'all'
+            },
+            transformResponse: data => {
+                const resp = JSON.parse(data)
+                const items = resp.data.items
+                let arr = []
+
+                for (const item of items){
+                    const pictures = []
+
+                    for (const subItem of item.pictures){
+                        pictures.push(subItem.img_src)
+                    }
+
+                    arr = arr.concat(pictures)
+                }
+
+                return arr
+            }
+        }))
+    }
+
+    const list = await axios.all(quest)
+    let pictures = []
+
+    for (const item of list){
+        pictures = pictures.concat(item.data)
+    }
+
+    return pictures
+}
+
+function writeFile(items){
+    let str = ''
+
+    for (const item of items){
+        str += `${ item }\n`
+    }
+
+    fs.writeFileSync(path.join(directory, `url.txt`), str)
+}
+
+function download(items){
+    let currentIndex = 0
+
+    from(items)
+        .pipe(
+            mergeMap(item => {
+                return from(axios.get(item, {
+                    responseType: 'arraybuffer',
+                    headers: {
+                        'User-Agent': userAgent
+                    }
+                }))
+            }, thread))
+        .subscribe(result => {
+            const filename = result.request.path.match(/[a-zA-Z0-9.]*$/gi)[0]
+            console.log(`Download: ${ (currentIndex += 1) }/${ items.length } ${ (currentIndex / items.length * 100).toFixed(2) }%`)
+            fs.writeFileSync(path.join(directory, filename), result.data)
+        })
+}
+
+async function main(){
+    const items = await findPictures()
+
+    fs.mkdirSync(directory, { recursive: true })
+    await writeFile(items)
+    if (outputFile){
+        download(items.slice(1, 15))
+    }
+}
+
+main()
